a327d81d94d7faa64ecbbf34a975a64bb69f9590
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2016-02-21 20:41:54 +0700

    add kira2 sistemwindowview

---
+++ src/nizami_cobacoba/sistemwindowview.h
@@ -0,0 +1,16 @@
+#ifndef sistemwindowview_h
+#define sistemwindowview_h
+
+class SistemWindowView{
+	//berisi informasi:
+	//batas-batas window
+	//batas-batas view
+	//Image alam
+	
+	//berisi method:
+	//pan: geser window
+	//zoom (in dan out): ubah ukuran window
+	//draw: memotong dari Image alam, translasi ke 0,0, skala ke ukuran view, translasi ke posisi view, lalu draw Image hasilnya
+};
+
+#endif
