42d650ce8c3a30f790b816550ec4dd4ed73f6740
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2016-02-21 20:48:34 +0700

    add kira2 main

---
+++ src/nizami_cobacoba/main.cpp
@@ -0,0 +1,15 @@
+//includes
+
+//kbhit untuk kontrol?
+
+int main(int argc, char *argv[] ){
+	//cek argumen
+
+	//buka dua buah ifstream dari nama file yang disuplai di argc argv
+	//itu untuk gambar dan tekstur
+
+	//buat sebuah objek sistemwindowview
+
+	//looping menerima kontrol untuk pan dan zoom serta menggambar
+	//jangan lupa ada kontrol untuk quit
+}
